f3b0e26709059f84dcf2dd509dc28606893c7046
suyud67 <suyudsetiawan@gmail.com>
2024-06-12 10:17:46 +0800

    update box shadow

--- src/components/socialMedia/index.tsx
+++ src/components/socialMedia/index.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import LinkButton from '../button/LinkButton';
 import { Github, Linkedin, Mail } from 'lucide-react';
+import { Email } from '../../assets/svg/mail';
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
   iconClassname?: string;
@@ -13,7 +14,7 @@ const SocialMedia = ({ className, iconClassname }: Props) => {
         label={
           <Linkedin
             className={clsx(
-              'hover:shadow-lg hover:shadow-slate-500/50 ease-in-out transition-all duration-500',
+              'hover:shadow-[0_10px_50px_rgba(10,102,194,0.5)] ease-in-out transition-all duration-500 bg-transparent',
               iconClassname
             )}
           />
@@ -27,7 +28,7 @@ const SocialMedia = ({ className, iconClassname }: Props) => {
         label={
           <Mail
             className={clsx(
-              'hover:shadow-lg hover:shadow-slate-500/50 ease-in-out transition-all duration-500',
+              'hover:shadow-[0_10px_50px_rgba(235,67,52,0.5)] ease-in-out transition-all duration-500',
               iconClassname
             )}
           />
@@ -43,7 +44,7 @@ const SocialMedia = ({ className, iconClassname }: Props) => {
         label={
           <Github
             className={clsx(
-              'hover:shadow-lg hover:shadow-slate-500/50 ease-in-out transition-all duration-500 ',
+              'hover:shadow-[0_10px_50px_rgba(231,236,243,0.5)] hover:drop-shadow-xl ease-in-out transition-all duration-500 ',
               iconClassname
             )}
           />
